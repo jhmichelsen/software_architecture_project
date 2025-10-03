24f46cc5f99dd2726c96d2c72009271ed05e753a
Jacob <jhmichelsen@hotmail.com>
2025-10-03 22:34:59 +0200

    Add initial project GreenhouseFactoryService

---
+++ Src/GreenhouseFactoryService/GreenhouseFactoryService/Controllers/FactoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenhouseFactoryService.Controllers;
+
+[ApiController]
+[Route("factory")]
+public class FactoryController(ILogger<FactoryController> logger) : ControllerBase
+{
+    [HttpGet("all")]
+    public IActionResult All()
+    {
+        return Ok();
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok();
+    }
+    
+    [HttpPost("create/{name}")]
+    public IActionResult Create(string name)
+    {
+        return Ok();
+    }
+    
+    [HttpPut("update/{id}")]
+    public IActionResult Update(int id)
+    {
+        return Ok();
+    }
+    
+    [HttpDelete("delete/{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Ok();
+    }
+}
