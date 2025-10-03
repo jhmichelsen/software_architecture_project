a825df35ab379ba58d89bd64bda80c510ceb0f34
Jacob <jhmichelsen@hotmail.com>
2025-10-03 20:24:26 +0200

    Add initial greenhouse manager project

---
+++ Src/GreenHouseApi/GreenHouseApi/Controllers/FactoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenHouseApi.Controllers;
+
+public class FactoryController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Src/GreenHouseApi/GreenHouseApi/Controllers/GreenHouseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenHouseApi.Controllers;
+
+public class GreenHouseController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
