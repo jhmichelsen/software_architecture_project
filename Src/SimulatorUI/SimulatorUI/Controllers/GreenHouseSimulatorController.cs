7867bfe790d0d544e543b3e48b43d35b2e59f96d
Jacob <jhmichelsen@hotmail.com>
2025-10-03 19:58:49 +0200

    Add first simulator endpoint

---
+++ Src/SimulatorUI/SimulatorUI/Controllers/GreenHouseSimulatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimulatorUI.Controllers;
+
+public class GreenHouseSimulatorController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
